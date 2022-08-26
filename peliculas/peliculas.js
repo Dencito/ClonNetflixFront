7b3d1046318e24743d5daf493c2db2a902d1d18c
Denar Padilla <denarpadilla.lionel@gmail.com>
2022-08-26 19:42:26 -0300

    Organizando las carpetas de las secciones

--- peliculas/peliculas.js
+++
@@ -1,8 +0,0 @@
-const labelBuscador = document.getElementById('labelBuscador')
-labelBuscador.addEventListener('click',()=>{
-    document.getElementById('buscador').style.display = 'block'
-    labelBuscador.style.position = 'absolute';
-    labelBuscador.style.top = '5px';
-    labelBuscador.style.left = '5px'
-})
-
